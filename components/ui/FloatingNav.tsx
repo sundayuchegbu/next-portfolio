f6bf3576f2d656676398644228f85f1fee44df7c
sundayuchegbu <sundayuchegbu10@gmail.com>
2025-05-01 13:32:36 +0100

    added for ..

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -47,8 +47,9 @@ export const FloatingNav = ({
         initial={{ opacity: 1, y: -100 }}
         animate={{ y: visible ? 0 : -100, opacity: visible ? 1 : 0 }}
         transition={{ duration: 0.2 }}
+        style={{ top: "2.5rem" }}
         className={cn(
-          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5 border-white/[0.2] bg-[#000319] items-center justify-center space-x-4",
+          "flex max-w-fit fixed inset-x-0 mx-auto border rounded-full shadow-[...] z-[5000] px-10 py-5 border-white/[0.2] bg-[#000319] items-center justify-center space-x-4",
           className
         )}
       >
